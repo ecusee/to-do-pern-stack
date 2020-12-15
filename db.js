e855cfb4e8cc9665d31da6f8be00ba6b3bc9a565
ecusee <erdobasogul@gmail.com>
2020-12-15 15:51:08 +0300

    db update

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
   user: "postgres",
-  password: "deneme123",
+  password: // postgres password //,
   host: "localhost",
   port: 5432,
   database: "perntodo",
